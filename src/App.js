6be1ae0a8aa3ce2f07111653a0168ac1dc2438f2
SD_heekyeong <phk08060@naver.com>
2021-06-15 10:12:00 +0900

    8.3

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import Counter from './Counter.js'
 import Info from './Info.js'
 
 const App = ()=> {
-  // return <Counter />;
-  const [visible, setVisible] = useState(false);
-  return (
-    <div>
-      <button onClick={()=>{
-        setVisible(!visible);
-      }}
-      >{visible?'숨기기':'보이기'}</button>
-      <hr />
-      {visible && <Info /> }
-    </div>
-  );
+  return <Counter />;
+  // const [visible, setVisible] = useState(false);
+  // return (
+  //   <div>
+  //     <button onClick={()=>{
+  //       setVisible(!visible);
+  //     }}
+  //     >{visible?'숨기기':'보이기'}</button>
+  //     <hr />
+  //     {visible && <Info /> }
+  //   </div>
+  // );
 };
 
 export default App;
